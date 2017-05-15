85f62750f943f98190d335c6c7adb2b1b97045f2
César Astudillo <cesar.astudillo@gmail.com>
2017-05-15 16:22:49 -0400

    Update FileTSP.java

--- src/tsp/FileTSP.java
+++ src/tsp/FileTSP.java
@@ -12,7 +12,13 @@ import java.util.ArrayList;
 
 /**
  *
- * @author JavierAros
+ * @author JavierAros, Cesar Astudillo
+ * Esta clase esta imcompleta. Usted debe agregar toda la funcionalidad.
+ * Esta clase se encargar de cargar el archivo que se pasa como parametros (ver linea #31).
+ * cada uno de los atributos, pertenece a una variable del archivo http://elib.zib.de/pub/mp-testdata/tsp/tsplib/tsp/bays29.tsp
+ * Usted debe leer el archivo y almacenar la informacion.
+ * para leer una secuencia de datos de una matriz en un archivo, recomiendo utilizar la clase StringTokenizer.
+ * su tarea sera investigar, como funciona esta clase y leer la matriz desde el archivo.
  */
 public class FileTSP {
     private String name;
